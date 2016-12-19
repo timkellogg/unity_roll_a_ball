5d7e1f05553bd218c2deabf1f7fc3c9a09230832
timkellogg <tim.kellogg@gmail.com>
2016-12-18 23:05:46 -0800

    finish first unity game!

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// called upon initialization
+	void Start()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	// called once per frame but it is gaurenteed to run after everything else
+	void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
